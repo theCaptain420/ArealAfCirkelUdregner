83a7c9bfbf71968aaa463e5e982de723bbe7ac70
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-09-21 10:55:05 +0200

    Indskrev commit test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 /* Creator: Mads Nielsen
     d. 21 - 09 - 2017
-    commit test
+    commit test2
  */
     public static void main(String[] args) {
         //læser input
