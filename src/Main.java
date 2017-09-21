1bf6921cdb52ba2b3ff72a9ac2b3394ec0df4fad
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-09-21 10:51:22 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+/* Creator: Mads Nielsen
+    d. 21 - 09 - 2017
+ */
+    public static void main(String[] args) {
+        //læser input
+        Scanner enterRadius = new Scanner(System.in);
+        System.out.println("Enter a number for radius :");
+        
+        double radius = enterRadius.nextDouble();
+        double areal = (radius*radius*Math.PI);
+
+        System.out.println("Arealet er , med "+radius+" som radius : " + areal);
+    }
+}
