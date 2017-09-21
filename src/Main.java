4779c3ea39662f8fcd55d000330a78f4cd0385cb
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-09-21 10:52:55 +0200

    Indskrev commit test

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
 /* Creator: Mads Nielsen
     d. 21 - 09 - 2017
+    commit test
  */
     public static void main(String[] args) {
         //læser input
         Scanner enterRadius = new Scanner(System.in);
         System.out.println("Enter a number for radius :");
-        
+
         double radius = enterRadius.nextDouble();
         double areal = (radius*radius*Math.PI);
 
